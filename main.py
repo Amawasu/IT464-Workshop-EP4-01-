f0cfb8c5cbeea60d304f09a0384baf016d70c44b
Amawasu <kkorya114@gmail.com>
2025-02-26 22:34:25 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello,Bu 1650704404'
+
+if __name__ == '__main__':
+    app.run(debug=True,port=80) 
